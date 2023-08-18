7c43c07aa05760c70c5416285a9bafca833e03d9
chenyioio <142574033+chenyioio@users.noreply.github.com>
2023-08-18 15:56:42 +0800

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,23 @@
+const projectData = {
+    // 码云账号（获取自己公开仓库用）
+    giteeName: "chenyoioi",
+    // 码云项目名字（因为公开项目也有很多可能不需要展示，所以这里的意义就是写上名字则渲染）
+    projectKeys: [
+        "NutssssIndex",
+    ],
+    // 请我喝咖啡的各位
+    support: [
+        {
+            name: "OiOi",
+            money: "100",
+            msg: "风月无关",
+            qq: "1"
+        },
+        {
+            name: "IoIo",
+            money: "100",
+            msg: "春秋不沾",
+            qq: "1"
+        }
+    ]
+};
